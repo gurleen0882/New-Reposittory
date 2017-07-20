d824660788497056ed64511894f00e74524cf364
gurleen0882 <gurleenkaur0882@gmail.com>
2017-07-20 10:55:57 +0530

    Add files via upload

---
+++ add-mul-div-invb1-Done/Default.aspx.vb
@@ -0,0 +1,15 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub btAdd_Click(sender As Object, e As EventArgs) Handles btAdd.Click, btDiv.Click, btMul.Click
+        If sender Is btAdd Then
+            lbl.Text = Convert.ToInt32(tbFirst.Text) + Convert.ToInt32(tbSecond.Text)
+        ElseIf sender Is btMul Then
+            lbl.Text = Convert.ToInt32(tbFirst.Text) * Convert.ToInt32(tbSecond.Text)
+        ElseIf sender Is btDiv Then
+            lbl.Text = Convert.ToInt32(tbFirst.Text) / Convert.ToInt32(tbSecond.Text)
+        End If
+
+    End Sub
+End Class
